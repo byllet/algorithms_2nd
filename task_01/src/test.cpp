dcbcb5e19cc48b2b6402a3d5700d9e29dc7c8310
byllet <109290692+byllet@users.noreply.github.com>
2023-09-09 10:45:23 +0000

    task01

--- task_01/src/test.cpp
+++ task_01/src/test.cpp
@@ -1,8 +1,13 @@
 
 #include <gtest/gtest.h>
 
+#include <vector>
+
 #include "topology_sort.hpp"
 
 TEST(TopologySort, Simple) {
-  ASSERT_EQ(1, 1);  // Stack []
+  Graph test1({{1, {2, 3}}, {2, {4, 5}}, {3, {}}, {4, {3, 5}}, {5, {}}});
+  ASSERT_EQ((test1.Topology_sort(1)),
+            (std::vector<unsigned int>{1, 2, 4, 5, 3}));
+  ASSERT_EQ((test1.Topology_sort(2)), (std::vector<unsigned int>{2, 4, 5, 3}));
 }
--- task_01/src/topology_sort.cpp
+++ task_01/src/topology_sort.cpp
@@ -1,1 +1,38 @@
 #include "topology_sort.hpp"
+
+#include <iostream>
+#include <vector>
+
+Graph::Graph(
+    std::unordered_map<unsigned int, std::vector<unsigned int>> nodes) {
+  nodes_.resize(nodes.size() + 1);
+  for (auto i = nodes.begin(); i != nodes.end(); ++i) {
+    nodes_[i->first] = i->second;
+  }
+}
+
+void Graph::Dfs(unsigned int current_node, std::vector<bool>& visited,
+                std::stack<unsigned int>& topology_stack) {
+  if (visited[current_node]) {
+    return;
+  }
+  visited[current_node] = true;
+  for (auto child : nodes_[current_node]) {
+    if (!visited[child]) {
+      Dfs(child, visited, topology_stack);
+    }
+  }
+  topology_stack.push(current_node);
+}
+
+std::vector<unsigned int> Graph::Topology_sort(unsigned int start_node) {
+  std::vector<bool> visited(nodes_.size(), false);
+  std::stack<unsigned int> topology_stack;
+  Dfs(start_node, visited, topology_stack);
+  std::vector<unsigned int> res;
+  for (unsigned int i = 0; i < topology_stack.size();) {
+    res.push_back(topology_stack.top());
+    topology_stack.pop();
+  }
+  return res;
+}
