249f3c0c1a49803a20f3b0c162ce2f4bb399048d
byllet <109290692+byllet@users.noreply.github.com>
2023-09-09 10:45:23 +0000

    task01

--- task_01/src/topology_sort.hpp
+++ task_01/src/topology_sort.hpp
@@ -1,1 +1,16 @@
 #pragma once
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+class Graph {
+ public:
+  Graph(std::unordered_map<unsigned int, std::vector<unsigned int>> nodes);
+  std::vector<unsigned int> Topology_sort(unsigned int start_node);
+
+ private:
+  void Dfs(unsigned int current_node, std::vector<bool>& colors,
+           std::stack<unsigned int>& topology_stack);
+  std::vector<std::vector<unsigned int>> nodes_;
+};
